b50c4a53514ef3dd6d9c5ed35711f8b6f9e4a1cf
bstiber <billstiber@gmail.com>
2016-07-04 15:36:16 -0400

    add file while.rb, practicing while loops

---
+++ while.rb
@@ -0,0 +1,10 @@
+# assigns x the value (variable) of 10
+x = 10
+# evaluates true until x is less than 0, thus once it reaches 0 and evaluates to false
+# the code will end (break).
+while x > 0
+  # prints x (over and over again, until x evaluates to false)
+  puts x
+  # decreases (x = x-1) the value of x by one
+  x -= 1
+end
