a65f35f030522e38ee9ce1217522d635dd5683ef
bstiber <billstiber@gmail.com>
2016-07-04 23:31:06 -0400

    add file and code

---
+++ each.rb
@@ -0,0 +1,7 @@
+for i in 1..25 do
+  puts "number #{i}"
+end
+
+(1..25).each do |i|
+  puts "number #{i} is my fav number"
+end
---
+++ new.rb
@@ -0,0 +1,7 @@
+for current_number in 1..100
+  puts "this is the current_number #{current_number}."
+end
+
+for number in 1..20
+  puts number
+end
